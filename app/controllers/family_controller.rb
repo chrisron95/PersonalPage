b6d8caabc0bf71e831ff50d934708f6f30802dcd
chrisron95 <chris@chrisheder.com>
2016-05-03 22:28:56 -0400

    Adjusted the nav bar for family pages

--- app/controllers/family_controller.rb
+++ app/controllers/family_controller.rb
@@ -1,2 +1,14 @@
 class FamilyController < ApplicationController
+  def show
+    if valid_page?
+      render template: "family/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/family/#{params[:page]}.html.erb"))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "pages#show", page: "index"
   get "/pages/*page" => "pages#show"
   get "/family" => "family#index"
+  get "/family/*page" => "family#show"
 end
